c9ebef0b7b8a919b4240028c48f16bd896aba772
MagnavoxTG <dominik.lohner92@gmx.at>
2019-11-18 09:42:22 +0100

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/cross_sum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cross_sum.dir/main.c.o"
+  "cross_sum"
+  "cross_sum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cross_sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
